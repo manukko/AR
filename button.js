4f16f10c8f76f783960f93a4242607582e94852e
manukko <45536493+manukko@users.noreply.github.com>
2018-12-09 21:38:46 +0100

    Update button.js

--- button.js
+++ button.js
@@ -1,4 +1,5 @@
 
-document.getElementById("theButton").addEventListener("click", function(){ console.log("qualcosa va"); });
+document.getElementById("theButton").addEventListener("click", function(){ 
+console.log("qualcosa va");
 document.getElementById("theButton").value="ora si che funziono<3";
 });
