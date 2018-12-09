9631875d5474f888c10cfdf90913e23326f18417
manukko <45536493+manukko@users.noreply.github.com>
2018-12-09 21:07:12 +0100

    Update button.js

--- button.js
+++ button.js
@@ -1,4 +1,5 @@
 
 Button b = document.getElementById("theButton").addEventListener("click", (e)=> {
-this.value="ora si che funziono<3";
+  console.log("qualcosa va");
+document.getElementById("theButton").value="ora si che funziono<3";
 })
