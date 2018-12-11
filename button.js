f3fea6a554c627a1ed1cd5d37375482e80463867
manukko <45536493+manukko@users.noreply.github.com>
2018-12-11 15:07:18 +0100

    Update button.js

--- button.js
+++ button.js
@@ -4,7 +4,7 @@ document.getElementById("theButton").addEventListener("click", function(){
 console.log("qualcosa va");
 otherWindow = window.open("https://manukko.github.io/Bilboard-1/", "otherWindow");
 document.getElementById("theButton").value="ora si che funziono<3";
-  setTimeout(function(){ console.log(otherWindow.postMessage("otherFunc", "*")); }, 3000);
+  setTimeout(function(){ console.log(otherWindow.postMessage("otherFunc", "*"));  }, 3000);
 
   counter++;
   
